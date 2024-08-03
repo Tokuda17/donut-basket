5816ad22e68a9b72d5e8efd94c0f6e50cea6edfb
Michael Tokuda <michael.tokuda17@gmail.com>
2024-08-03 11:23:16 -0400

    Transition from page to page complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 // src/App.js
 
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import HomePage from "./pages/HomePage";
 import MenuPage from "./pages/MenuPage";
 import AboutPage from "./pages/AboutPage";
@@ -11,20 +18,24 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="app-container">
-      <Router>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/menu" element={<MenuPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/order-online" element={<OrderOnlinePage />} />
-          </Routes>
-        </div>
-      </Router>
+      <Navbar />
+      <div>
+        <TransitionGroup>
+          <CSSTransition key={location.key} timeout={1000} classNames="fade">
+            <Routes location={location}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/menu" element={<MenuPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/order-online" element={<OrderOnlinePage />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
     </div>
   );
 }
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,6 +4,14 @@ function AboutPage() {
   return (
     <div>
       <h1>AboutPage</h1>
+      <p>
+        Veniam deserunt nostrud qui exercitation ea pariatur amet qui dolor et
+        velit. Consectetur labore aliqua reprehenderit commodo ut cupidatat.
+        Nostrud ut id consectetur enim ullamco sit aliquip quis culpa. Aliqua
+        ullamco culpa aute eu pariatur id non deserunt duis cupidatat sint
+        excepteur. Sunt irure quis veniam qui id est dolor. Duis laboris
+        deserunt enim consectetur pariatur elit Lorem nulla.
+      </p>
     </div>
   );
 }
