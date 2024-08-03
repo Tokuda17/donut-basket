f029623a85efab1b294027cf3df2d99e85ebd88e
Michael Tokuda <michael.tokuda17@gmail.com>
2024-08-03 15:07:28 -0400

    Start to menu page

---
+++ src/components/Ribbon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Ribbon.css";
+
+function Ribbon({ text }) {
+  return <div class="ribbon">{text}</div>;
+}
+
+export default Ribbon;
--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,10 +1,54 @@
-import React from "react";
+import { React, useState } from "react";
+import Ribbon from "../components/Ribbon";
+import "./MenuPage.css";
 
 function MenuPage() {
+  const [selectedMenu, setSelectedMenu] = useState("breakfast");
+
+  const handleMenuChange = (menu) => {
+    setSelectedMenu(menu);
+  };
+
+  const menuItems = {
+    breakfast: ["Pancakes", "Waffles", "Omelette"],
+    lunch: ["Burger", "Salad", "Sandwich"],
+    dinner: ["Steak", "Pasta", "Pizza"],
+  };
+
   return (
-    <div>
-      <h1>Menu PAGE</h1>
-    </div>
+    <>
+      <div className="menu-page-top">
+        <Ribbon text="Menu" />
+      </div>
+      <div>
+        <div>
+          <button
+            className="menu-chioces"
+            onClick={() => handleMenuChange("breakfast")}
+          >
+            Breakfast
+          </button>
+          <button
+            className="menu-chioces"
+            onClick={() => handleMenuChange("lunch")}
+          >
+            Lunch
+          </button>
+          <button
+            className="menu-chioces"
+            onClick={() => handleMenuChange("dinner")}
+          >
+            Dinner
+          </button>
+        </div>
+
+        <div>
+          {menuItems[selectedMenu].map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
